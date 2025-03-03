c9743c8f92a55838e6616ceff4f68249b44c0123
duwuzil <deuziljoestar@gmail.com>
2025-03-03 15:50:54 +0100

    coucou toi

--- epopee du poulet/Programmation/Arduino/mini exos/lienlinux/lienlinux.ino
+++ epopee du poulet/Programmation/Arduino/mini exos/lienlinux/lienlinux.ino
@@ -2,11 +2,11 @@
 
 const byte numChars = 1024;
 char receivedChars[numChars];
-char tempChars[numChars];        // temporary array for use when parsing
+char tempChars[numChars];  // temporary array for use when parsing
 
 
-      // variables to hold the parsed data
-char messageFromPC[numChars] = {0};
+// variables to hold the parsed data
+char messageFromPC[numChars] = { 0 };
 int integerFromPC = 0;
 float floatFromPC = 0.0;
 int rwop = 0;
@@ -16,92 +16,90 @@ boolean newData = false;
 //============
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("This demo expects 3 pieces of data - text, an integer and a floating point value");
-    Serial.println("Enter data in this style <HelloWorld, 12, 24.7>  ");
-    Serial.println();
+  Serial.begin(9600);
+  Serial.println("This demo expects 3 pieces of data - text, an integer and a floating point value");
+  Serial.println("Enter data in this style <HelloWorld, 12, 24.7>  ");
+  Serial.println();
 }
 
 //============
 
 void loop() {
-    recvWithStartEndMarkers();
-    if (newData == true) {
-        strcpy(tempChars, receivedChars);
-            // this temporary copy is necessary to protect the original data
-            //   because strtok() used in parseData() replaces the commas with \0
-        parseData();
-        showParsedData();
-        newData = false;
-    }
-    if(rwop == 1){
-      Serial.println(String(messageFromPC));
-    }
-    
-    delay(100);
+  recvWithStartEndMarkers();
+  if (newData == true) {
+    strcpy(tempChars, receivedChars);
+    // this temporary copy is necessary to protect the original data
+    //   because strtok() used in parseData() replaces the commas with \0
+    parseData();
+    showParsedData();
+    newData = false;
+  }
+  if (rwop == 1) {
+    Serial.println(String(messageFromPC));
+  }
+
+  delay(100);
 }
 
 //============
 
 void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
 
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
 
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars) {
-                    ndx = numChars - 1;
-                }
-            }
-            else {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
         }
-
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
+      } else {
+        receivedChars[ndx] = '\0';  // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
     }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
 }
 
 //============
 
-void parseData() {      // split the data into its parts
+void parseData() {  // split the data into its parts
 
-    char * strtokIndx; // this is used by strtok() as an index
+  char* strtokIndx;  // this is used by strtok() as an index
 
-    strtokIndx = strtok(tempChars,",");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    integerFromPC = atoi(strtokIndx);     // convert this part to an integer
+  strtokIndx = strtok(tempChars, ",");  // get the first part - the string
+  strcpy(messageFromPC, strtokIndx);    // copy it to messageFromPC
 
-    strtokIndx = strtok(NULL, ",");
-    floatFromPC = atof(strtokIndx);     // convert this part to a float
+  strtokIndx = strtok(NULL, ",");    // this continues where the previous call left off
+  integerFromPC = atoi(strtokIndx);  // convert this part to an integer
 
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    rwop = atoi(strtokIndx);
+  strtokIndx = strtok(NULL, ",");
+  floatFromPC = atof(strtokIndx);  // convert this part to a float
 
+  strtokIndx = strtok(NULL, ",");  // this continues where the previous call left off
+  rwop = atoi(strtokIndx);
 }
 
 //============
 
 void showParsedData() {
-    Serial.print("Message ");
-    Serial.println(messageFromPC);
-    Serial.print("Integer ");
-    Serial.println(integerFromPC);
-    Serial.print("Float ");
-    Serial.println(floatFromPC);
+  Serial.print("Message ");
+  Serial.println(messageFromPC);
+  Serial.print("Integer ");
+  Serial.println(integerFromPC);
+  Serial.print("Float ");
+  Serial.println(floatFromPC);
 }
